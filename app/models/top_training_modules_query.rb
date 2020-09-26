4047104c944b4a35581f61d5637460a737c0fca1
Piotr Pańko <piotr.panko@netguru.com>
2020-09-26 18:52:53 +0200

    Count only TrainingModuleActivities created in last days

--- app/models/top_training_modules_query.rb
+++ app/models/top_training_modules_query.rb
@@ -1,17 +1,21 @@
 class TopTrainingModulesQuery
-  def initialize(limit: 10)
+  def initialize(limit: 10, days: 30.days)
     @limit = limit
+    @days = days
   end
 
   # SELECT training_modules.*, COUNT(training_module_activities.training_module_id) as count
   # FROM "training_modules"
   # INNER JOIN "training_module_activities"
   #   ON "training_module_activities"."training_module_id" = "training_modules"."id"
+  # WHERE (training_module_activities.created_at > '2020-08-27 16:50:14.519939')
   # GROUP BY training_modules.id
   # ORDER BY count DESC LIMIT 10
   def call
     TrainingModule.select('training_modules.*, COUNT(training_module_activities.training_module_id) as count')
                   .joins(:training_module_activities)
+                  .where('training_module_activities.activity_name = ?', 'execute')
+                  .where('training_module_activities.created_at > ?', DateTime.current - days)
                   .group('training_modules.id')
                   .order('count DESC')
                   .limit(limit)
@@ -19,5 +23,5 @@ class TopTrainingModulesQuery
 
   private
 
-  attr_reader :limit
+  attr_reader :limit, :days
 end

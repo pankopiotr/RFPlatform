81453b3b49727884eedd8ef40e99b00f977aba05
Piotr Pańko <piotr.panko@netguru.com>
2020-09-26 17:07:02 +0200

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,14 @@
 categories = [Category.create(name: 'Category1'), Category.create(name: 'Category2')]
 
-courses = (1..10).map do |x|
+courses = (1...10).map do |x|
   Course.create(name: "Course#{x}", category: categories.sample)
 end
 
-100.times do |x|
+training_modules = (1...100).times do |x|
   number_of_courses = rand(2) + 1
   TrainingModule.create(name: "TrainingModule#{x}", courses: courses.sample(number_of_courses))
+end
+
+1000.times do |x|
+  TrainingModuleActivity.create(activity_name: 'execute', training_module: training_modules.sample)
 end

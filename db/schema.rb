b1ffac48422951771128415bbeb3ee323dd6a43d
Piotr Pańko <piotr.panko@netguru.com>
2020-09-26 14:21:16 +0200

    Generate Category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_26_114906) do
+ActiveRecord::Schema.define(version: 2020_09_26_120343) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "courses", force: :cascade do |t|
     t.string "name"

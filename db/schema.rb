7fee6718b0c8bedf799fa23d42cc83d6b3313dce
Piotr Pańko <piotr.panko@netguru.com>
2020-09-26 13:32:54 +0200

    Generate Course model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_26_103039) do
+ActiveRecord::Schema.define(version: 2020_09_26_112822) do
+
+  create_table "courses", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "training_modules", force: :cascade do |t|
     t.string "name"
